c04e334f2b021a8a2a24d8470c30df402a9da535
Sakuya <502566796@qq.com>
2017-06-26 00:05:52 +0800

    修改eslint检测目录

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # Boilerplate
-React单页面开发环境。
+React单页面开发环境，[测试页面预览](https://sakuyakun.github.io/React-Webpack-Boilerplate/)。
+目前依旧存在许多不完善的地方，如果有错误或者好的搭建建议请联系我。
 
-- React15 React-Redux5 React-Router15 ES6
-- 使用 CSSModule 与 Sass 进行CSS开发 使用PostCSS
+- 主要技术栈 React15 React-Redux5 React-Router15 SASS ES6/7
+- 使用 React CSS Modules 与 PostCSS 进行处理
 - 使用 Webpack3 进行处理打包
 - 使用 Hot Module Replacement 热加载
 - 使用 Eslint 代码检测与 Prettier 格式化
-- 代码文件压缩，添加MD5
+- 代码压缩，文件添加MD5缓存处理
+
+# Todo
+- [ ] Router按需加载
+- [ ] 解决ESlint检测问题
+- [ ] 交互动画处理
+- [ ] Mock与数据请求处理
 
 # Run
 `yarn run start` 开发环境启动
@@ -14,7 +21,3 @@ React单页面开发环境。
 `yarn run build` 部署打包
 
 `yarn run lint` 代码检测
-
-# Todo
-- [ ] router按需加载
-- [ ] 解决eslint检测问题
--- src/utils/routerbundle.jsx
+++ src/utils/routerbundle.jsx
@@ -1,33 +1,33 @@
 // 暂未开启按需加载路由功能 此文件保留
 // routerbundle 用于路由异步加载组件
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Bundle extends Component {
-    state = {
-        mod: null
+  state = {
+    mod: null
+  };
+  componentWillMount() {
+    this.load(this.props);
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.load !== this.props.load) {
+      this.load(nextProps);
     }
-    componentWillMount() {
-        this.load(this.props)
+  }
+  load(props) {
+    this.setState({
+      mod: null
+    });
+    props.load(mod => {
+      this.setState({
+        mod: mod.default ? mod.default : mod
+      });
+    });
+  }
+  render() {
+    if (!this.state.mod) {
+      return false;
     }
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.load !== this.props.load) {
-            this.load(nextProps)
-        }
-    }
-    load(props) {
-        this.setState({
-            mod: null
-        })
-        props.load((mod) => {
-            this.setState({
-                mod: mod.default ? mod.default : mod
-            })
-        })
-    }
-    render() {
-        if (!this.state.mod) {
-          return false
-        }
-        return this.props.children(this.state.mod)
-    }
+    return this.props.children(this.state.mod);
+  }
 }
