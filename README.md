d0b6f158001a47fffdeed079f593307e27c71d8e
Sakuya <502566796@qq.com>
2017-07-26 23:07:47 +0800

    移除路由按需加载

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 React单页面开发环境，[测试页面预览](https://sakuyakun.github.io/React-Webpack-Boilerplate/)。
 
-目前依旧存在许多不完善的地方，如果有错误或者好的建议请联系我。计划日后会用这个环境来写一套自己的组件库。
+如果有错误或者好的建议请联系我。计划日后会用这个环境来写一套自己的组件库。
 
 # Feature
 - 主要技术栈 React15 React-Redux5 React-Router4 SASS ES6/7
@@ -14,8 +14,8 @@ React单页面开发环境，[测试页面预览](https://sakuyakun.github.io/Re
 - 代码压缩，文件添加MD5缓存处理
 
 # Run
-`yarn run start` 开发环境启动
+`run run start` 开发环境启动
 
-`yarn run build` 部署打包
+`run run build` 部署打包
 
-`yarn run lint` 代码检测
+`run run lint` 代码检测
--- src/routers.jsx
+++ src/routers.jsx
@@ -4,17 +4,6 @@ import styles from "./style.scss";
 // 使用BrowserRouter需要nginx做处理
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-// 按需加载
-// import Bundle from './utils/routerbundle'
-// import AboutContainer from 'bundle-loader?lazy!./views/about.jsx';
-
-// 异步引入
-// const About = () => (
-//   <Bundle load={AboutContainer}>
-//     { (About) => <About /> }
-//   </Bundle>
-// )
-
 // Router
 import Navigation from "./views/navigation";
 import Index from "./views/index";
